c0ff2918e7ef8f00f6b9fa3a13bec2e51190de74
eva <eva@machiavelli.dhcp.wustl.edu>
2018-01-17 14:02:02 -0600

    studio 0 part 1

---
+++ heartbeat/heartbeat.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  long n = millis();
+  Serial.print(n/1000);
+  Serial.println(" sec have elapsed");
+  delay(1000);
+}
